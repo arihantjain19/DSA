f9043d2067af5deb40b90882cf045fb38f0efd6f
arihantjain19 <arihantjain1905@gmail.com>
2026-08-22 19:45:04 +0530

    Update and rename 94 | Leetcode - Binary Tree Inorder Traversal (Recursive).py to 94 | Leetcode - Binary Tree Inorder Traversal.py

--- 94 | Leetcode - Binary Tree Inorder Traversal.py
+++ 94 | Leetcode - Binary Tree Inorder Traversal.py
@@ -6,6 +6,20 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#ITERATIVE APPROACH
+        res=[]
+        stack=[]
+        cur=root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur=cur.left
+            cur=stack.pop()
+            res.append(cur.val)
+            cur=cur.right
+        return res
+        
+#RECURSIVE APPROACH
         res=[]
         def inorder(root):
             if not root:
