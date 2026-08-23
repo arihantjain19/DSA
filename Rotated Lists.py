e59905a0385a5902fec5ad24b024737a111e1457
arihantjain19 <arihantjain1905@gmail.com>
2026-08-23 20:07:01 +0530

    Rotated Lists.py

---
+++ Rotated Lists.py
@@ -0,0 +1,24 @@
+#QUESTION
+"""
+Problem - Rotated Lists
+You are given list of numbers, obtained by rotating a sorted list an unknown number of times. 
+Write a function to determine the minimum number of times the original sorted list was rotated to obtain the given list. 
+Your function should have the worst-case complexity of O(log N), where N is the length of the list. You can assume that all the numbers in the list are unique.
+Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list [0, 2, 3, 4, 5, 6, 9] 3 times.
+We define "rotating a list" as removing the last element of the list and adding it before the first element. 
+E.g. rotating the list [3, 2, 4, 1] produces [1, 3, 2, 4].
+"Sorted list" refers to a list where the elements are arranged in the increasing order e.g. [1, 3, 5, 7]
+"""
+#SOLUTION
+def rotation_count(nums)
+    left=0
+    right=len(nums)-1
+    while left<=right:
+          mid=(left+right)//2
+          if nums[mid]>nums[right]:
+              left=mid+1
+          else:
+              right=mid+1
+          return left
+    nums=[5,6,9,0,2,3,4]
+    print(rotation_count(nums))
